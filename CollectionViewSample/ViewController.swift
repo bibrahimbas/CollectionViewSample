6ee7adef610966470c18981db6c598ca6d438dd2
Belma Ibrahimbaş <belmaibrahimbas@belma-macbook-pro.local>
2018-12-28 16:25:05 +1100

    Initial Commit

---
+++ CollectionViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewSample
+//
+//  Created by Belma İbrahimbaş on 28.12.2018.
+//  Copyright © 2018 Belma Ibrahimbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
